a39540dc305b0147398bb67edc4efdcc09371e5c
Selva Sabapathy <selva.sabapathy@gmail.com>
2021-03-13 12:29:12 -0500

    Initial commit

---
+++ src/test/java/home/sabapathy/cryptozoo/CryptozooApplicationTests.java
@@ -0,0 +1,13 @@
+package home.sabapathy.cryptozoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptozooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
